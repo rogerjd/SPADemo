df981b204acc7c7697c6adb7d43ee151c1eeea2b
rjpd@netzero.com <rogerjd>
2019-04-13 00:05:56 -0400

    Add project files.

---
+++ SPADemo/Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPADemo.Model
+{
+    public class Todo
+    {
+        public string Text { get; set; }
+        public bool Done { get; set; }
+    }
+}
