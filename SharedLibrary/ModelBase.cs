2a0fe034e2ab1e18aa6ade9e9e8c6d8799d534d9
rjpd@netzero.com <rogerjd>
2019-04-13 15:17:58 -0400

    Validation, start

---
+++ SharedLibrary/ModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary
+{
+    public class ModelBase
+    {
+        private Dictionary<string, Dictionary<string, string>> _errors =
+            new Dictionary<string, Dictionary<string, string>>();
+
+        public void AddError(string fieldName, string ruleName, string errorText)
+        {
+
+        }
+    }
+}
