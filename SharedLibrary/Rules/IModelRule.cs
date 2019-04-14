f1a3b7389bc39fb8ee61dd4e48f602a97b721c40
rjpd@netzero.com <rogerjd>
2019-04-14 17:11:20 -0400

    MaxLengthRule

--- SharedLibrary/Rules/IModelRule.cs
+++ SharedLibrary/Rules/IModelRule.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SharedLibrary
+namespace SharedLibrary.Rules
 {
     public interface IModelRule
     {
-        ValidationResult Validate(string fieldName, object fieldValue)
+        ValidationResult Validate(string fieldName, object fieldValue);
     }
 }
