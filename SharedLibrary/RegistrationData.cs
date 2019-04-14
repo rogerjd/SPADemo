311b0bcbfe2ea6b51485697d2a69dc4279e406e5
rjpd@netzero.com <rogerjd>
2019-04-14 14:47:07 -0400

    Validation, reg data

---
+++ SharedLibrary/RegistrationData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary
+{
+    class RegistrationData: ModelBase
+    {
+        [RequiredRule]
+        [MaxLengthRule(50)]
+        public string FirstName { get; set; }
+
+        [RequiredRule]
+        [MaxLengthRule(50)]
+        public string LastName { get; set; }
+    
+        [EmailRule]
+        public string Email { get; set; }
+
+        [PhoneRule]
+        public string Phone { get; set; }
+    }
+}
