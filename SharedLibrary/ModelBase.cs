353d3c374bdef0f25ac0cf20a5f7daa7715973f8
rjpd@netzero.com <rogerjd>
2019-04-13 21:41:04 -0400

    Validation, ModelBase, wip

--- SharedLibrary/ModelBase.cs
+++ SharedLibrary/ModelBase.cs
@@ -10,7 +10,29 @@ namespace SharedLibrary
 
         public void AddError(string fieldName, string ruleName, string errorText)
         {
+            if (!_errors.ContainsKey(fieldName))
+            {
+                _errors.Add(fieldName, new Dictionary<string, string>());
+            }
+            if (_errors[fieldName].ContainsKey(ruleName))
+            {
+                _errors[fieldName].Remove(ruleName);
+            }
+            _errors[fieldName].Add(ruleName, errorText);
+            OnModelChanged();
+        }
 
+        public void RemoveError(string fieldName, string ruleName)
+        {
+            if (!_errors.ContainsKey(fieldName)
+            {
+                _errors.Add(fieldName, new Dictionary<string, string>());
+            }
+            if (_errors[fieldName].ContainsKey(ruleName))
+            {
+                _errors[fieldName].Remove(ruleName);
+            }
+            OnModelChanged();
         }
     }
 }
